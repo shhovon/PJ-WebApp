2af2143e2d1cf076c2dee42eea7ff40ac49756f6
Shovon <shovon.zzz5@gmail.com>
2024-01-21 23:30:07 +0600

    files added

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult getAllEmployee()
+        {
+            using (CRUDEntities dbcon = new CRUDEntities())
+            {
+                var lstEmp = dbcon.Emp_Info.ToList().OrderBy(ein => ein.EmpName);
+                return new JsonResult { Data = new { lstEmployee = lstEmp }, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        public ActionResult createEmp(int RegID, string EmpID, string EmpName, string DepartmentName, string SectionName, string Designation, int Salary)
+        {
+            using (CRUDEntities dbcon = new CRUDEntities())
+            {
+                Emp_Info emp_Info = new Emp_Info();
+                emp_Info.RegID = RegID;
+                emp_Info.EmpID = EmpID;
+                emp_Info.EmpName = EmpName;
+                emp_Info.DepartmentName = DepartmentName;
+                emp_Info.SectionName = SectionName;
+                emp_Info.Designation = Designation;
+                emp_Info.Salary = Salary;
+
+                dbcon.Emp_Info.Add(emp_Info);
+                dbcon.SaveChanges();
+            }
+            return null;
+        }
+
+        public ActionResult createEmployee()
+        {
+          
+            return View();
+        }
+         public ActionResult showEmployee()
+        {
+          
+            return View();
+        }
+
+
+        public ActionResult createEmpp()
+        {
+            return View();
+            // return RedirectToAction("ViewEmp");
+
+        }
+        public ActionResult ViewEmp()
+        {
+            return View();
+        }
+        public ActionResult editEmp(int RegID, string EmpID, string EmpName, string DepartmentName, string SectionName, string Designation, int Salary)
+        {
+            using (CRUDEntities dbcon = new CRUDEntities())
+            {
+                var emp_Info = dbcon.Emp_Info.Where(ein => ein.RegID == RegID).FirstOrDefault();
+                emp_Info.EmpID = EmpID;
+                emp_Info.EmpName = EmpName;
+                emp_Info.DepartmentName = DepartmentName;
+                emp_Info.SectionName = SectionName;
+                emp_Info.Designation = Designation;
+                emp_Info.Salary = Salary;
+
+                dbcon.Emp_Info.Add(emp_Info);
+                dbcon.Entry(emp_Info).State = EntityState.Modified;
+                dbcon.SaveChanges();
+            }
+            return null;
+        }
+
+        public ActionResult editEmpp()
+        {
+            ViewBag.Message = "Edit Employee";
+
+            return View();
+        }
+          public ActionResult editEmployee()
+        {
+            ViewBag.Message = "Edit Employee";
+
+            return View();
+        }
+
+
+        public ActionResult deleteEmp(int RegID)
+        {
+            using (CRUDEntities dbcon = new CRUDEntities())
+            {
+                var emp_Info = dbcon.Emp_Info.Where(ein => ein.RegID == RegID).FirstOrDefault();
+
+                dbcon.Emp_Info.Remove(emp_Info);
+                dbcon.SaveChanges();
+            }
+            return null;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Scripts/jsController/testController.js
@@ -0,0 +1,70 @@
+﻿var module = angular.module('testModule', []); //, ['ngSanitize', 'ui.select', 'kendo.directives', 'ui.filters']);
+
+module.controller('testController', function ($scope, $http) {
+
+    // Populate Unit
+    $scope.getAllEmployee = function () {
+        $http.get('/Home/getAllEmployee').then(function (data) {
+            $scope.lstEmp = data.data.lstEmployee;
+            //alert(angular.toJson($scope.lstEmp));
+            //console.log($scope.lstEmp);
+        }, function (error) {
+            $.notify('Error is ' + error.data.message, { globalPosition: 'top right', className: error.data.type });
+        });
+    }
+
+    $scope.createEmployee = function () {
+        $http({
+            url: '/Home/createEmp',
+            method: 'POST',
+            data: JSON.stringify({
+                RegID: $scope.RegID, EmpID: $scope.EmpID, EmpName: $scope.EmpName,
+                DepartmentName: $scope.DepartmentName, SectionName: $scope.SectionName,
+                Designation: $scope.Designation, Salary: $scope.Salary
+            })
+        }).then(function (data) {
+            debugger;
+            $scope.getAllEmployee();
+            window.location = '../Home/ViewEmp';
+        }, function (data) {
+        });
+    }
+    $scope.setEditEmployee = function (emp) {
+        $scope.RegID = emp.RegID;
+        $scope.EmpID = emp.EmpID;
+        $scope.EmpName = emp.EmpName;
+        $scope.DepartmentName = emp.DepartmentName;
+        $scope.SectionName = emp.SectionName;
+        $scope.Designation = emp.Designation;
+        $scope.Salary = emp.Salary;
+    }
+
+    $scope.editEmployee = function () {
+        $http({
+            url: '/Home/editEmp',
+            method: 'POST',
+            data: JSON.stringify({
+                RegID: $scope.RegID, EmpID: $scope.EmpID, EmpName: $scope.EmpName,
+                DepartmentName: $scope.DepartmentName, SectionName: $scope.SectionName,
+                Designation: $scope.Designation, Salary: $scope.Salary
+            })
+        }).then(function (data) {
+            $scope.getAllEmployee();
+        }, function (data) {
+        });
+    }
+
+
+    $scope.DeleteEmployee = function (emp) {
+        $http({
+            url: '/Home/deleteEmp',
+            method: 'POST',
+            data: JSON.stringify({
+                RegID: $scope.RegID
+            })
+        }).then(function (data) {
+            $scope.getAllEmployee();
+        }, function (data) {
+        });
+    }
+});
